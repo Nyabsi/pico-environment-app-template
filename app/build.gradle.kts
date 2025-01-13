5e5bfa23cc68d69c9d75137b9b5861f55709e601
Nyabsi <nyabsi@sovellus.cc>
2025-01-13 22:12:22 +0200

    Revert "enable build minifying to prevent debug boilerplate"
    This reverts commit 0edbd5df02cd144512d19d187bff907b22dc37c2.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,14 +19,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-        debug {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
